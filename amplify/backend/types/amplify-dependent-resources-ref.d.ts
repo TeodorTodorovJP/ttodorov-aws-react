b6e1adc28f3aa2c5a29c48060590a64a869bfdf9
Teodor Todorov <ttodorov.jp@gmail>
2024-04-07 07:03:20 +0300

    Created a BE for this app, to act as a connection with other BE services

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "connectReact": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "connectFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
